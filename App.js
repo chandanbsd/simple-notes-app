c081a94e4b48bb6d972ddbe4413f85a96703e720
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2022-07-28 00:48:01 -0400

    Camera Functionality added

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 import React, { useEffect, useState } from "react";
+import { Camera } from "expo-camera";
 
 export default function App() {
+  const [cameraAccess, setCameraAccess] = useState(false);
   const [text, setText] = useState(null);
   const [notes, setNotes] = useState([]);
+  const [showCamera, setShowCamera] = useState(false);
+  const [photo, setPhoto] = useState(null);
+  const [camera, setCamera] = useState(null);
 
   useEffect(() => {
-    console.log(notes);
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setCameraAccess(status === "true");
+    })();
   }, [notes]);
+
+  const takePicture = async () => {
+    const tempPhoto = await camera.takePictureAsync(null);
+    setPhoto(tempPhoto.uri);
+  };
 
   return (
     <View style={styles.container}>
@@ -26,12 +39,32 @@ export default function App() {
           setText(null);
 
           if (text != null && text.length > 0) {
-            setNotes([...notes, { text: text }]);
+            setNotes([...notes, { text: text, img: photo }]);
+            setPhoto(null);
+            console.log(notes);
           } else {
             Alert.alert("Cannot Save", "Please add a description or an image");
           }
         }}
       />
+      <Button title="Show Camera" onPress={() => setShowCamera(!showCamera)} />
+
+      {showCamera ? (
+        <Camera
+          type={true}
+          style={styles.camera}
+          ref={(r) => {
+            setCamera(r);
+          }}
+        >
+          <View style={styles.cameraView}>
+            <Text> Flip This </Text>
+            <Button title="Take Picture" onPress={() => takePicture()} />
+          </View>
+        </Camera>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 }
@@ -50,5 +83,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     width: "90%",
     height: "40%",
+  },
+  camera: {
+    height: "40%",
+    width: "90%",
   },
 });
