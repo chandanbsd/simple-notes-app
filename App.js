1ef4cd103519d2f4c94ab4870a3d93ac197c38d1
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2022-07-27 21:55:31 -0400

    Text Notes Component Implemented

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState(null);
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    console.log(notes);
+  }, [notes]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Simple Notes App</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.note}
+        multiline={true}
+        onChangeText={setText}
+        value={text}
+      />
+      <Button
+        title="Save"
+        onPress={() => {
+          setText(null);
+
+          if (text != null && text.length > 0) {
+            setNotes([...notes, { text: text }]);
+          } else {
+            Alert.alert("Cannot Save", "Please add a description or an image");
+          }
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +39,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 30,
+  },
+  note: {
+    backgroundColor: "#fff",
+    width: "90%",
+    height: "40%",
   },
 });
